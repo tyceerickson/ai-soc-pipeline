8076e4807b53840ae0c2f130d8ceabc3ace832dd
tyceerickson <tyceerickson@gmail.com>
2026-06-22 12:07:24 -0600

    Update VPS host in sync_nginx.sh script

--- pipeline/sync_nginx.sh
+++ pipeline/sync_nginx.sh
@@ -10,7 +10,7 @@
 
 set -uo pipefail
 
-VPS_HOST="root@100.89.15.57"
+VPS_HOST="<sync-user>@<vps-host>"
 VPS_PORT="2222"
 SSH_KEY="/home/terickson/.ssh/vps_sync"
 SSH_OPTS="-p ${VPS_PORT} -i ${SSH_KEY} -o BatchMode=yes -o ConnectTimeout=10 -o StrictHostKeyChecking=accept-new"
